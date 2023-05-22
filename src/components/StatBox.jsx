4f7cb1005eb6a8ed902864f1dae76c4505101ce9
mat943 <madhukarmayank93@gmail.com>
2023-05-23 00:58:05 +0530

    create progress circle and statbox component

---
+++ src/components/StatBox.jsx
@@ -0,0 +1,42 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../scenes/global/theme";
+import ProgressCircle from "./ProgressCircle";
+
+const StatBox = ({ title, subtitle, icon, progress, increase }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box width="100%" m="0 30px">
+      <Box display="flex" justifyContent="space-between">
+        <Box>
+          {icon}
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            sx={{ colo: colors.grey[100] }}
+          >
+            {title}
+          </Typography>
+        </Box>
+        <Box>
+          <ProgressCircle progress={progress} />
+        </Box>
+      </Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h5" sx={{ colo: colors.greenAccent[500] }}>
+          {subtitle}
+        </Typography>
+        <Typography
+          variant="h5"
+          fontWeight="italic"
+          sx={{ colo: colors.greenAccent[600] }}
+        >
+          {increase}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default StatBox;
